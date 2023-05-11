e4bc9d9276a0c82157b7e0fbe442a3a21208c6ba
Hetvi Vora <hetvivora3007@gmail.com>
2023-05-11 19:05:18 +0530

    added some mediaquery

--- src/Pages/PortfolioPage/Portfolio.js
+++ src/Pages/PortfolioPage/Portfolio.js
@@ -24,44 +24,25 @@ const Portfolio = () => {
             <div className='contactpage-subcontainer'>
                 <div className='portfoliopage-title-container'>
                     <h1><span className='span-tag'>MY </span>PORTFOLIO</h1>
-                    {/* <span className='poetfoliopage-background-title'>WORKS</span> */}
                 </div>
                 <div className='myportfolio-container'>
                     <Link className='portfolio-link' to='https://hetvi-react-accessory-store.netlify.app/'>
                         <img src={accessory} />
-                        {/* <div className='portfolio-name-container'>
-                                    <h3>Accessory Store</h3>
-                                </div> */}
                     </Link>
                     <Link className='portfolio-link' to='https://hetvivora-weatherapp.netlify.app/'>
                         <img src={weatherapp} />
-                        {/* <div className='portfolio-name-container'>
-                                    <h3></h3>
-                                </div> */}
                     </Link>
                     <Link className='portfolio-link' to='https://hetvi-simplecalculator.netlify.app/'>
                         <img src={calculator} />
-                        {/* <div className='portfolio-name-container'>
-                                    <h3></h3>
-                                </div> */}
                     </Link>
                     <Link className='portfolio-link' to='https://hetvi-todowebapp.netlify.app/'>
                         <img src={todowebapp} />
-                        {/* <div className='portfolio-name-container'>
-                                    <h3></h3>
-                                </div> */}
                     </Link>
                     <Link className='portfolio-link' to='https://hetvi-stopwatch.netlify.app/'>
                         <img src={stopwatch} />
-                        {/* <div className='portfolio-name-container'>
-                                    <h3></h3>
-                                </div> */}
                     </Link>
                     <Link className='portfolio-link' to='https://hetvi-myfirstwebsite.netlify.app/'>
                         <img src={myfirstwebsite} />
-                        {/* <div className='portfolio-name-container'>
-                                    <h3></h3>
-                                </div> */}
                     </Link>
                 </div>
             </div>
